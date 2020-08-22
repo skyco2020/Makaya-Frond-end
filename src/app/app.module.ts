b1eedf88b3ee54613d1964aaed018fdb8490e1b6
Pradel <prade5_16@hotmail.com>
2020-08-22 18:02:21 -0300

    Clean code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,36 +3,28 @@ import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { SignInComponent } from './user/sign-in/sign-in.component';
 import { HomeComponent } from './home/home.component';
 import { from } from 'rxjs';
 import {NgbModalModule} from '@ng-bootstrap/ng-bootstrap';
 import {ReactiveFormsModule} from '@angular/forms';
-import { SignUpComponent } from './user/sign-up/sign-up.component';
 import { UserComponent } from './user/user.component';
 import {RouterModule} from '@angular/router';
 import { appRoutes } from './routes';
 import { ToastrModule } from 'ngx-toastr';
 import { AuthGuard } from './auth/auth.guard';
-import { LoginComponent } from './Login/login/login.component';
 import { AccountComponent } from './account/account.component';
 import { RegisterpaymentComponent } from './registerpayment/registerpayment.component';
 import {NgxStripeModule } from 'ngx-stripe';
-import { ModalComponent } from './modal/modal.component';
 import { FooterComponent } from './footer/footer.component';
 import { FinishpaymentComponent } from './finishpayment/finishpayment.component';
 import { NonepaymentComponent } from './nonepayment/nonepayment.component';
 @NgModule({
   declarations: [
     AppComponent,
-    SignInComponent,
     HomeComponent,
-    SignUpComponent,
     UserComponent,
-    LoginComponent,
     AccountComponent,
     RegisterpaymentComponent,
-    ModalComponent,
     FooterComponent,
     FinishpaymentComponent,
     NonepaymentComponent,
@@ -47,7 +39,7 @@ import { NonepaymentComponent } from './nonepayment/nonepayment.component';
     RouterModule.forRoot(appRoutes),
     ToastrModule.forRoot()
     ],
-  entryComponents: [ModalComponent],
+  entryComponents: [],
   providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router'
 import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
-import { SignUpComponent } from './user/sign-up/sign-up.component';
-import { SignInComponent } from './user/sign-in/sign-in.component';
 import {AccountComponent} from './account/account.component';
 import {RegisterpaymentComponent} from './registerpayment/registerpayment.component';
 import {FinishpaymentComponent} from './finishpayment/finishpayment.component';
@@ -17,12 +15,7 @@ export const appRoutes: Routes = [
        canActivate: [AuthGuard]
     },
     {
-        path: 'signup', component: UserComponent,
-        children: [{ path: '', component: SignUpComponent }]
-    },
-    {
-        path: 'login', component: UserComponent,
-        children: [{ path: '', component: SignInComponent }]
+        path: 'login', component: UserComponent
     },
     {
         path: 'account', component: AccountComponent
