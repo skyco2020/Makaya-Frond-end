e731eb65ec204a9ef784aa65465716d0e9fdaa53
= <MESSEROUXHILAIRE@GMAIL.COM>
2020-08-30 22:47:39 -0300

    done with register account and logo

--- src/app/registerpayment/registerpayment.component.ts
+++ src/app/registerpayment/registerpayment.component.ts
@@ -2,7 +2,12 @@ import { Component, OnInit, Input } from '@angular/core';
 import { SkycoService } from '../services/skyco.service';
 import { TaskService } from '../services/task.service';
 import { HttpErrorResponse } from '@angular/common/http';
-import { StripeService, Elements, Element as StripeElement, ElementsOptions } from 'ngx-stripe';
+import {
+  StripeService,
+  Elements,
+  Element as StripeElement,
+  ElementsOptions,
+} from 'ngx-stripe';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Token } from '../Classes/token';
 import { PaymentIntentDto } from '../Classes/payment-intent-dto';
@@ -35,7 +40,6 @@ let opacity;
   selector: 'app-registerpayment',
   templateUrl: './registerpayment.component.html',
   styleUrls: ['./registerpayment.component.css'],
-
 })
 export class RegisterpaymentComponent implements OnInit {
   usr = new SkycoUser();
@@ -58,7 +62,7 @@ export class RegisterpaymentComponent implements OnInit {
   elements: Elements;
   card: StripeElement;
   elementoption: ElementsOptions = {
-    locale: 'en'
+    locale: 'en',
   };
   PlanSelect: any;
   StripeTest: FormGroup;
@@ -70,8 +74,8 @@ export class RegisterpaymentComponent implements OnInit {
     private paymentService: PaymentServiceService,
     private fb: FormBuilder,
     private stripeSvc: StripeService,
-    public modalService: NgbModal) {
-  }
+    public modalService: NgbModal
+  ) {}
 
   ngOnInit(): void {
     // tslint:disable-next-line: only-arrow-functions
@@ -79,8 +83,7 @@ export class RegisterpaymentComponent implements OnInit {
     funcionCustom();
     if (localStorage.getItem('mail') === null) {
       this.router.navigate(['/']);
-    }
-    else {
+    } else {
       $('#mail-2').val(localStorage.getItem('mail'));
       $('#confirm_mail-2').val(localStorage.getItem('mail'));
       this.EnableMail();
@@ -89,45 +92,44 @@ export class RegisterpaymentComponent implements OnInit {
       $('.cardhidden').show();
       $('.hiddenfooter').hide();
       this.GetFormStripe();
-     }
+    }
   }
 
-  EnableMail(){
+  EnableMail() {
     $('#mail-2').attr('readonly', true);
     $('#confirm_mail-2').attr('readonly', true);
   }
-  GetFormStripe(){
+  GetFormStripe() {
     this.StripeTest = this.fb.group({
-      name: ['', Validators.required]
+      name: ['', Validators.required],
     });
 
-    this.stripeSvc.elements(this.elementoption)
-      .subscribe(elements => {
-        this.elements = elements;
-        if (!this.card) {
-          this.card = this.elements.create('card', {
-            style: {
-              base: {
-                iconColor: '#666EE8',
-                color: '#31325F',
-                lineHeight: '40px',
-                fontWeight: 300,
-                fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-                fontSize: '18px',
-                '::placeholder': {
-                  color: '#CFD7E0'
-                }
-              }
-            }
-          });
-          this.card.mount('#card-element');
-        }
-      });
+    this.stripeSvc.elements(this.elementoption).subscribe((elements) => {
+      this.elements = elements;
+      if (!this.card) {
+        this.card = this.elements.create('card', {
+          style: {
+            base: {
+              iconColor: '#666EE8',
+              color: '#31325F',
+              lineHeight: '40px',
+              fontWeight: 300,
+              fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+              fontSize: '18px',
+              '::placeholder': {
+                color: '#CFD7E0',
+              },
+            },
+          },
+        });
+        this.card.mount('#card-element');
+      }
+    });
   }
 
-  GetLocation(){
+  GetLocation() {
     // tslint:disable-next-line: only-arrow-functions
-    $.getJSON('https://ipapi.co/jsonp?callback=?', function(data) {
+    $.getJSON('https://ipapi.co/jsonp?callback=?', function (data) {
       $('#country').val(data.country_name);
       $('#code_country').val(data.country_code);
       $('#province').val(data.region);
@@ -138,13 +140,10 @@ export class RegisterpaymentComponent implements OnInit {
     });
   }
 
-
-
   GetSelectPlan() {
     if (selecttyp === undefined) {
       $('.error_plan').html('You have to select a plan');
-    }
-    else {
+    } else {
       this.LoadData(true);
       $('.ste2').val(2);
       this.LoadDataObject();
@@ -157,55 +156,56 @@ export class RegisterpaymentComponent implements OnInit {
     if (name === '') {
       $('.error_fullname').html('Full name');
       // return false;
-    }
-    else {
-      this.LoadData(true);
-      this.stripeSvc
-        .createToken(this.card, { name })
-        .subscribe(result => {
-          if (result.token) {
-            console.log(result.token);
-            const paymentIntentDto: PaymentIntentDto = {
-              // tslint:disable-next-line: radix
-              AccountId: parseInt(localStorage.getItem('IdUser')),
-              CardId: result.token.card.id,
-              Description: this.PlanSelect.nickname,
-              Email: localStorage.getItem('mail'),
-              IDStripePrice: this.PlanSelect.id,
-              fullname: name,
-              idPaymentIntent: 0,
-              state: 1,
-              stripeTokenId: result.token.id
-            };
+    } else {
+      /* lo comemtamos para no mostrar el loading */
+      // this.LoadData(true);
+      this.stripeSvc.createToken(this.card, { name }).subscribe(
+        (result) => {
+          this.GetNextPage();
+          /* lo comemtamos para que pueda hacer lo de front-end sin bombadear la base de datos */
+          // if (result.token) {
+          //   console.log(result.token);
+          //   const paymentIntentDto: PaymentIntentDto = {
+          //     // tslint:disable-next-line: radix
+          //     AccountId: parseInt(localStorage.getItem('IdUser')),
+          //     CardId: result.token.card.id,
+          //     Description: this.PlanSelect.nickname,
+          //     Email: localStorage.getItem('mail'),
+          //     IDStripePrice: this.PlanSelect.id,
+          //     fullname: name,
+          //     idPaymentIntent: 0,
+          //     state: 1,
+          //     stripeTokenId: result.token.id
+          //   };
 
-            this.paymentService.Post(paymentIntentDto).subscribe(
-              (data: any) => {
-                this.LoadData(false);
-                this.succes = data;
-                this.GetNextPage();
-                this.LoadRedirect();
-              },
-              (err: HttpErrorResponse) => {
-                this.LoadData(false);
-                this.error = err.error.ErrorDescription;
-              }
-            ),
-            // tslint:disable-next-line: no-unused-expression
-            (err: HttpErrorResponse) => {
-              this.LoadData(false);
-              this.error = err.error.ErrorDescription;
-            };
-            this.error = undefined;
-          } else if (result.error) {
-            this.LoadData(false);
-            this.error = result.error.message;
-          }
+          //   this.paymentService.Post(paymentIntentDto).subscribe(
+          //     (data: any) => {
+          //       this.LoadData(false);
+          //       this.succes = data;
+          //       this.GetNextPage();
+          //       this.LoadRedirect();
+          //     },
+          //     (err: HttpErrorResponse) => {
+          //       this.LoadData(false);
+          //       this.error = err.error.ErrorDescription;
+          //     }
+          //   ),
+          //   // tslint:disable-next-line: no-unused-expression
+          //   (err: HttpErrorResponse) => {
+          //     this.LoadData(false);
+          //     this.error = err.error.ErrorDescription;
+          //   };
+          //   this.error = undefined;
+          // } else if (result.error) {
+          //   this.LoadData(false);
+          //   this.error = result.error.message;
+          // }
         },
-          (err: HttpErrorResponse) => {
-            this.error = err.error.ErrorDescription;
-          });
+        (err: HttpErrorResponse) => {
+          this.error = err.error.ErrorDescription;
+        }
+      );
     }
-
   }
   HideMessage(msg) {
     $('.' + msg).html('');
@@ -214,7 +214,7 @@ export class RegisterpaymentComponent implements OnInit {
 
   OnSubmit() {
     if (this.validateField() === true) {
-      this.LoadData(true);
+      // this.LoadData(true);
       this.usr = new SkycoUser();
       //#region User
       // tslint:disable-next-line: radix
@@ -226,7 +226,7 @@ export class RegisterpaymentComponent implements OnInit {
       this.usr.UpdatedBy = $('#name-2').val() + ' ' + $('#surname-2').val();
       this.usr.Country = $('#country').val();
       this.usr.province = $('#province').val();
-      this.usr.city =  $('#city').val();
+      this.usr.city = $('#city').val();
       //#endregion
 
       //#region Adrress
@@ -252,29 +252,39 @@ export class RegisterpaymentComponent implements OnInit {
       //#region Location
       Account.location.Latitude = $('#latitude').val();
       Account.location.Longitude = $('#longitude').val();
-      Account.location.CreatedBy = $('#name-2').val() + ' ' + $('#surname-2').val();
+      Account.location.CreatedBy =
+        $('#name-2').val() + ' ' + $('#surname-2').val();
       Account.location.CreatedAt = new Date();
       Account.location.Voided = 1;
       //#endregion
 
       //#region Country
-      Account.location.Country.CountryCode = $('#code_country').val() != null ? $('#code_country').val() : 'None identificate';
-      Account.location.Country.CountryName = $('#country').val() != null ? $('#country').val() : 'None identificate';
-      Account.location.Country.CreatedBy =  $('#name-2').val() + ' ' + $('#surname-2').val();
+      Account.location.Country.CountryCode =
+        $('#code_country').val() != null
+          ? $('#code_country').val()
+          : 'None identificate';
+      Account.location.Country.CountryName =
+        $('#country').val() != null ? $('#country').val() : 'None identificate';
+      Account.location.Country.CreatedBy =
+        $('#name-2').val() + ' ' + $('#surname-2').val();
       Account.location.Country.CreatedAt = new Date();
       Account.location.Country.Voided = 1;
 
       //#region Province
       const Prove = new Province();
-      Prove.ProvinceName = $('#province').val() != null ? $('#province').val() : 'None identificate';
-      Prove.CreatedBy =  $('#name-2').val() + ' ' + $('#surname-2').val();
+      Prove.ProvinceName =
+        $('#province').val() != null
+          ? $('#province').val()
+          : 'None identificate';
+      Prove.CreatedBy = $('#name-2').val() + ' ' + $('#surname-2').val();
       Prove.CreatedAt = new Date();
       Prove.Voided = 1;
 
       //#region City
       const cit = new City();
-      cit.CityName = $('#city').val() != null ? $('#city').val() : 'None identificate';
-      cit.CreatedBy =  $('#name-2').val() + ' ' + $('#surname-2').val();
+      cit.CityName =
+        $('#city').val() != null ? $('#city').val() : 'None identificate';
+      cit.CreatedBy = $('#name-2').val() + ' ' + $('#surname-2').val();
       cit.CreatedAt = new Date();
       cit.Voided = 1;
 
@@ -287,32 +297,32 @@ export class RegisterpaymentComponent implements OnInit {
 
       this.usr.Skyco_Account.push(Account);
       //#endregion
-
-      this.userService.Put(this.usr)
-        .subscribe((data: any) => {
-          // tslint:disable-next-line: no-debugger
-          this.usr = this.usr;
-          IStep2 = true;
-          this.GetNextPage();
-        },
-          (err: HttpErrorResponse) => {
-            this.LoadData(false);
-            this.errorconnection = 'Error occured while trying to proxy.';
-          });
+      this.GetNextPage();
+      /* lo comemtamos para que pueda hacer lo de front-end sin bombadear la base de datos */
+      // this.userService.Put(this.usr).subscribe(
+      //   (data: any) => {
+      //     // tslint:disable-next-line: no-debugger
+      //     this.usr = this.usr;
+      //     IStep2 = true;
+      //     this.GetNextPage();
+      //   },
+      //   (err: HttpErrorResponse) => {
+      //     this.LoadData(false);
+      //     this.errorconnection = 'Error occured while trying to proxy.';
+      //   }
+      // );
     }
-
   }
- LoadData(val){
-  setTimeout(() => {
-    this.load = val;
-  }, 0);
- }
+  LoadData(val) {
+    setTimeout(() => {
+      this.load = val;
+    }, 0);
+  }
   GetPlan() {
     const filter = '?count=3';
-    this.paymentService.GetAll(filter).
-      subscribe((data: any) => {
-        this.plan = data.Result.Result.data;
-      });
+    this.paymentService.GetAll(filter).subscribe((data: any) => {
+      this.plan = data.Result.Result.data;
+    });
   }
 
   CarSubmit(item, index) {
@@ -323,7 +333,7 @@ export class RegisterpaymentComponent implements OnInit {
         display: 'block',
         position: 'relative',
         'border-color': 'initial',
-        'border-style': 'initial'
+        'border-style': 'initial',
       });
     }
 
@@ -331,7 +341,7 @@ export class RegisterpaymentComponent implements OnInit {
       display: 'block',
       position: 'relative',
       'border-color': '#EF7F1A',
-      'border-style': 'dotted'
+      'border-style': 'dotted',
     });
     $('.error_plan').html('');
     this.PlanSelect = item;
@@ -371,14 +381,14 @@ export class RegisterpaymentComponent implements OnInit {
       $('.error_password-2').html('diference password');
       return false;
     } else if ($('#password-2').val().length < 6) {
-      $('.error_password-2').html('The password can\'t be less than 6 characters');
+      $('.error_password-2').html(
+        "The password can't be less than 6 characters"
+      );
       return false;
-    }
-    else if (selecttyp === undefined && $('.ste2').val() === '2') {
+    } else if (selecttyp === undefined && $('.ste2').val() === '2') {
       $('.error_plan').html('You have to select a plan');
       return false;
-    }
-    else {
+    } else {
       return true;
     }
   }
@@ -389,7 +399,7 @@ export class RegisterpaymentComponent implements OnInit {
     next_fs.show();
     current_fs.css({
       display: 'none',
-      position: 'relative'
+      position: 'relative',
     });
   }
 
@@ -399,29 +409,27 @@ export class RegisterpaymentComponent implements OnInit {
     if (!result) {
       $('.input_e_mail').val('');
       return false;
-    }
-    else {
+    } else {
       return true;
     }
   }
-  LoadDataObject(){
+  LoadDataObject() {
     setTimeout(() => {
       this.GetNextPage();
       this.LoadData(false);
     }, 5000);
-   }
-   LoadRedirect(){
+  }
+  LoadRedirect() {
     setTimeout(() => {
       this.router.navigate(['/account']);
     }, 30000);
-   }
+  }
 }
-
 
 function funcionCustom() {
   // tslint:disable-next-line: only-arrow-functions
-  $(document).ready(function() {
-    $('.next').click(function() {
+  $(document).ready(function () {
+    $('.next').click(function () {
       current_fs = $(this).parent();
       next_fs = $(this).parent().next();
       if (selecttyp === undefined && IStep2 === true) {
@@ -432,30 +440,33 @@ function funcionCustom() {
       }
     });
 
-    $('.previous').click(function() {
+    $('.previous').click(function () {
       current_fs = $(this).parent();
       previous_fs = $(this).parent().prev();
-      $('#progressbar li').eq($('fieldset').index(current_fs)).removeClass('active');
+      $('#progressbar li')
+        .eq($('fieldset').index(current_fs))
+        .removeClass('active');
 
       // show the previous fieldset
       previous_fs.show();
 
       // hide the current fieldset with style
-      current_fs.animate({ opacity: 0 }, {
-        step(now) {
-          // for making fielset appear animation
-          opacity = 1 - now;
+      current_fs.animate(
+        { opacity: 0 },
+        {
+          step(now) {
+            // for making fielset appear animation
+            opacity = 1 - now;
 
-          current_fs.css({
-            display: 'none',
-            position: 'relative'
-          });
-          previous_fs.css({ opacity });
-        },
-        duration: 600
-      });
-    }
-    );
-
+            current_fs.css({
+              display: 'none',
+              position: 'relative',
+            });
+            previous_fs.css({ opacity });
+          },
+          duration: 600,
+        }
+      );
+    });
   });
 }
