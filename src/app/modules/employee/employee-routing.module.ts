56b7d571567ce341cd6833cf445fcfd0e5f9d0e1
Pradel <prade5_16@hotmail.com>
2020-09-05 14:15:59 -0300

    security

--- src/app/modules/employee/employee-routing.module.ts
+++ src/app/modules/employee/employee-routing.module.ts
@@ -1,7 +1,7 @@
-import { NgModule } from '@angular/core';
+|import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListemployeComponent } from './listemploye/listemploye.component';
-securityimport { AuthGuard } from 'src/app/auth/auth.guard';
+import { AuthGuard } from 'src/app/auth/auth.guard';
 
 
 const routes: Routes = [
