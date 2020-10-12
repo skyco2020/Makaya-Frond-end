e64032f7d3f01d05e21628565c643e2079798031
Pradel <prade5_16@hotmail.com>
2020-10-12 00:35:44 -0300

    Add and modify perfil

--- src/app/profilmodule/profil/profil.component.ts
+++ src/app/profilmodule/profil/profil.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TaskService } from '../../services/task.service';
 import { PerfilService } from '../../services/perfil.service';
+import{Perfil} from '../../Classes/perfil';
 
 import decode from 'jwt-decode';
+declare var $: any;
 
 @Component({
   selector: 'app-profil',
@@ -24,7 +26,47 @@ export class ProfilComponent implements OnInit {
 
     this.serperfil.GetAll(filter).subscribe((data: any) => {
       this.profils = data.ResourceList;
+      if(this.profils.length >= 5){
+        $('.add').css({
+          display:'none'
+        });
+      }
     });
+  }
+  Add(){
+    $('.profiles').css({
+      display:'none'
+    })
+    $('.addprofile').css({
+      display:'block'
+    })
+  }
+  Cancel(){
+    $('.profiles').css({
+      display:'flex'
+    })
+    $('.addprofile').css({
+      display:'none'
+    })
+  }
+  Save(){
+    if($('#name').val() == ""){
+      $('#errorname').css({
+        display:'block'
+      })
+    }
+    else{
+      let prof = new Perfil();
+      prof.idPerfil = $('#idprofil').val();
+      prof.name =$('#name').val();
+      prof.complete = $('#complete').val()
+
+      // this.serperfil.Post(prof).
+      // subscribe((data: any) => {
+      //   this.ngOnInit();
+      //   this. Cancel();
+      // })
+    }
   }
   ShowHome(profil) {
     debugger;
--- src/app/profilmodule/updateprofil/updateprofil.component.ts
+++ src/app/profilmodule/updateprofil/updateprofil.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { Route } from '@angular/router';
+import { Router } from '@angular/router';
+import { TaskService } from '../../services/task.service';
+import { PerfilService } from '../../services/perfil.service';
+import{Perfil} from '../../Classes/perfil';
+
+import decode from 'jwt-decode';
+import { from } from 'rxjs';
+declare var $: any;
+
 
 @Component({
   selector: 'app-updateprofil',
@@ -7,18 +15,76 @@ import { Route } from '@angular/router';
   styleUrls: ['./updateprofil.component.css'],
 })
 export class UpdateprofilComponent implements OnInit {
-  constructor() {}
+  profils: any;
+  constructor(
+    private userService: TaskService,
+    private serperfil: PerfilService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    const tokenPayload = decode(this.userService.getJwtToken());
+    let filter = '?AccountId=' + tokenPayload.Idaccount;
+
+    this.serperfil.GetAll(filter).subscribe((data: any) => {
+      this.profils = data.ResourceList;
+
+    });
     trye();
+  }
+
+  UpdateProfil(profil){
+    $('.profiles').css({
+      display:'none'
+    })
+    $('.modify').css({
+      display:'block'
+    })
+    $('#name').val(profil.name);
+    $('#idprofil').val(profil.idPerfil);
+    $('#complete').val(profil.complete)
+  }
+
+  Cancel(){
+    $('.profiles').css({
+      display:'flex'
+    })
+    $('.modify').css({
+      display:'none'
+    })
+  }
+  Save(){
+    if($('#name').val() == ""){
+      $('#errorname').css({
+        display:'block'
+      })
+    }
+    else{
+      let prof = new Perfil();
+      prof.idPerfil = $('#idprofil').val();
+      prof.name =$('#name').val();
+      prof.complete = $('#complete').val()
+
+      this.serperfil.Put(prof).
+      subscribe((data: any) => {
+        this.ngOnInit();
+        this. Cancel();
+      })
+    }
   }
 }
 function trye() {
-  document.querySelector('#edit').addEventListener('click', function (e) {
-    // alert(this.getAttribute('data-id'));
-    document.getElementById('profiles').style.display = 'none';
-    document.getElementById('modal').style.height = '100vh!important';
-    document.getElementById('modal').style.width = '100%!important';
-    document.getElementById('modal').style.position = 'absolute';
-  });
+  debugger;
+  //  const email = document.querySelector("profile-user");
+  // $(".edit").on("click", function(){
+  //   debugger;
+  // })
+  // document.querySelector('#edit').addEventListener('click', function (e) {
+  //   debugger;
+  //   // alert(this.getAttribute('data-id'));
+  //   document.getElementById('profiles').style.display = 'none';
+  //   document.getElementById('modal').style.height = '100vh!important';
+  //   document.getElementById('modal').style.width = '100%!important';
+  //   document.getElementById('modal').style.position = 'absolute';
+  // });
 }
