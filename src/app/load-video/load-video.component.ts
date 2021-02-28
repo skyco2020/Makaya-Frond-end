b0190e98a7d3d2f43b67d8452b95f2d807bfbad3
= <MESSEROUXHILAIRE@GMAIL.COM>
2021-02-27 22:05:41 -0300

    fixing modal wide home

--- src/app/load-video/load-video.component.ts
+++ src/app/load-video/load-video.component.ts
@@ -1,15 +1,72 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
 @Component({
   selector: 'app-load-video',
   templateUrl: './load-video.component.html',
-  styleUrls: ['./load-video.component.css']
+  styleUrls: ['./load-video.component.scss']
 })
 export class LoadVideoComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+    document.querySelector('.close-modal').addEventListener('click',()=>{
+      $('.modal-upload-video').css({
+        display : 'none'
+      })    
+      $('.modal-upload-video-delete').css({
+        display : 'none'
+      })  
+      // this.initForm();
+      // this.iscreated = true;
+    });
+    document.querySelector('.fa-times-circle').addEventListener('click',()=>{   
+      console.log('estoy en delete..');
+      $('.modal-upload-video-delete').css({
+        display : 'none',
+        background: 'red'
+
+      })  
+      // this.initForm();
+      // this.iscreated = true;
+    });
+
+    document.querySelector('.btn-add-upload-video').addEventListener('click',()=>{
+      $('.modal-upload-video').css({
+        display : 'block'
+      })
+     
+      // this.iscreated = true;
+      // this.initForm();
+      // $('#type-plan').focus();
+    });
+    // document.querySelectorAll('.fa-pen-square').forEach(elem =>{
+    //   elem.addEventListener('click',()=>{
+    //     $('.modal-upload-video').css({
+    //       display : 'block'
+    //     })
+    //     // this.iscreated = true;
+    //     // this.initForm();
+    //   });
+    // });
+
+    document.querySelectorAll('.fa-trash').forEach(el =>{
+      el.addEventListener('click',()=>{
+        console.log('modal upload-video delte');
+        $('.modal-upload-video-delete').css({
+          display : 'block'
+        });
+        // this.iscreated = true;
+        // this.initForm();
+      });
+    })
+  }
+  CloseModal(){
+    $('.modal-plan-delete').css({
+      display : 'none'
+    })
+    // this.initDeleteForm();
   }
 
 }
--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from 'src/app/admin/admin.component';
 import { PlanComponent } from 'src/app/plan/plan.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: PlanComponent,
+    component: AdminComponent ,
     data: {
-      title: 'Perfil'
+      title: 'Admin'
     }
    },
    {
